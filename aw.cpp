be6154f0f581c75beafc985d9d2ae4672d9233e9
hanonymous89 <96468681+hanonymous89@users.noreply.github.com>
2022-02-27 13:17:04 +0900

    Add files via upload

--- aw.cpp
+++ aw.cpp
@@ -1,6 +1,7 @@
 ﻿#include <windows.h>
 #include <string>
 #include <unordered_map>
+#include <functional>
 namespace h {
     enum WM_CMD {
         BTN_PUSH=4649
@@ -93,6 +94,77 @@ namespace h {
         winc.lpszClassName = name;
         return RegisterClass(&winc);
     }
+    //class MouseUpTimer {
+    //private:
+    //    std::function<void(HWND,UINT,UINT,DWORD)> normal, end;
+    //    VOID CALLBACK timer(HWND hwnd, UINT uMsg, UINT idEvent, DWORD dwTime) {
+    //        if (0 <= GetAsyncKeyState(VK_LBUTTON)) {
+    //            //SendMessage(hwnd, WM_LBUTTONUP, 0, 0);
+    //            end(hwnd,uMsg,idEvent,dwTime);
+    //            KillTimer(hwnd, idEvent);
+    //            return;
+    //        }
+    //        normal(hwnd, uMsg, idEvent, dwTime);
+    //    }
+    //    MouseUpTimer(){}
+    //    auto reset(decltype(normal) normal, decltype(end) end) {
+    //        this->normal = normal;
+    //        this->end = end;
+    //        return timer;
+    //    }
+    //public:
+    //    MouseUpTimer(const MouseUpTimer&) = delete;
+    //    MouseUpTimer& operator=(const MouseUpTimer&) = delete;
+    //    MouseUpTimer(const MouseUpTimer&&) = delete;
+    //    MouseUpTimer& operator=(const MouseUpTimer&&) = delete;
+    //    static auto getInstance(decltype(normal) normal, decltype(end) end) {
+    //        static MouseUpTimer instance;
+    //        return instance.reset(normal, end);
+    //    }
+
+    //};
+    enum class mouseUpTimerControl :UINT {
+        UP,
+        INTERVAL=100
+    };
+
+    class mouseUpTimer {
+    private:
+        static std::unordered_map < UINT, std::pair<std::function<void(HWND, UINT, UINT, DWORD)>, std::function<void(HWND, UINT, UINT, DWORD)> >  > data;
+        static VOID CALLBACK timer(HWND hwnd, UINT uMsg, UINT idEvent, DWORD dwTime) {
+            if (0 <= GetAsyncKeyState(VK_LBUTTON)) {
+                data[idEvent].second(hwnd, uMsg, idEvent, dwTime);
+                KillTimer(hwnd, idEvent);
+                return;
+            }
+           data[idEvent].first(hwnd, uMsg, idEvent, dwTime);
+        }
+        mouseUpTimer() {}
+    public:
+        mouseUpTimer(const mouseUpTimer&) = delete;
+        mouseUpTimer& operator=(const mouseUpTimer&) = delete;
+        mouseUpTimer(const mouseUpTimer&&) = delete;
+        mouseUpTimer& operator=(const mouseUpTimer&&) = delete;
+        static auto &getInstance() {
+            static mouseUpTimer instance;
+            return instance;
+        }
+        auto &set(const UINT id,const typename decltype(data)::mapped_type::first_type first,decltype(first) second) {
+            //data.emplace(id, first,second );
+            data[id] = { first,second };
+            //data.insert_or_assign(id,);
+            return getInstance();
+        }
+        //auto& set(const typename decltype(data)::key_type key, const typename decltype(data)::mapped_type value) {
+        //    data.insert_or_assign(key,value);
+        //    return getInstance();
+        //}
+        auto& setTimer(const HWND hwnd,const UINT id,const UINT interval) {
+            SetTimer(hwnd,id,interval,(TIMERPROC)timer);
+            return getInstance();
+        }
+    };
+    decltype(mouseUpTimer::data) mouseUpTimer::data;
 };
 VOID CALLBACK mouseUpTimer(HWND hwnd, UINT uMsg, UINT idEvent, DWORD dwTime) {
     if (0<=GetAsyncKeyState(VK_LBUTTON)) {
@@ -115,11 +187,11 @@ LRESULT CALLBACK btnProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
         InvalidateRect(hwnd, nullptr, true);
         UpdateWindow(hwnd);
         if (GetParent(hwnd) == nullptr)break;
-        SendMessage(GetParent(hwnd), WM_COMMAND,h::WM_CMD::BTN_PUSH,0);
+        SendMessage(GetParent(hwnd), WM_COMMAND, h::WM_CMD::BTN_PUSH, 0);
         break;
     case WM_LBUTTONDOWN:
         data[hwnd] = true;
-        SetTimer(hwnd, 4649, 100, (TIMERPROC)mouseUpTimer);
+        h::mouseUpTimer::getInstance().setTimer(hwnd, static_cast<UINT>(h::mouseUpTimerControl::UP), static_cast<UINT>(h::mouseUpTimerControl::INTERVAL));
         InvalidateRect(hwnd, nullptr, true);
         UpdateWindow(hwnd);
         break;
@@ -141,21 +213,17 @@ LRESULT CALLBACK btnProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
     }
     return DefWindowProc(hwnd, msg, wp, lp);
 }
+LRESULT CALLBACK scrollProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
+
+    return DefWindowProc(hwnd, msg, wp, lp);
+}
 LRESULT CALLBACK wndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
-    static HWND child;
     switch (msg) {
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
     case WM_CREATE:
-        child=CreateWindow(TEXT("btn"), TEXT("BTN"), WS_VISIBLE | WS_CHILD | WS_CLIPCHILDREN | WS_CLIPSIBLINGS, 0, 0, 100, 100,hwnd, nullptr, LPCREATESTRUCT(lp)->hInstance, nullptr);
-        break;
-    case WM_SIZE:
-    {
-        RECT rect;
-        GetClientRect(hwnd, &rect);
-        MoveWindow(child, 0, 0,rect.right,rect.bottom,true);
-    }
+        CreateWindow(TEXT("btn"), TEXT("BTN"), WS_VISIBLE | WS_CHILD | WS_CLIPCHILDREN | WS_CLIPSIBLINGS, 0, 0, 100, 100, hwnd, nullptr, LPCREATESTRUCT(lp)->hInstance, nullptr);
         break;
     }
     return DefWindowProc(hwnd, msg, wp, lp);
@@ -165,6 +233,13 @@ int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,PSTR lpCmdLine,in
 	MSG msg;
     h::baseStyle(wndProc, L"main");
     h::baseStyle(btnProc,L"btn");
+    h::mouseUpTimer::getInstance().set(static_cast<UINT>(h::mouseUpTimerControl::UP),
+        [](HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {},
+        [](HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
+            SendMessage(hwnd, WM_LBUTTONUP, 0, 0);
+        }
+
+    );
     CreateWindow(TEXT("main"), TEXT("Main"), WS_VISIBLE | WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, nullptr, nullptr, hInstance, nullptr);
 	while (GetMessage(&msg, nullptr, 0, 0)) {
 		TranslateMessage(&msg);
